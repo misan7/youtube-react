089a63034840bcadbbf151e37090359559c7cd89
Sergio MT <sergio.mt88@gmail.com>
2017-10-31 13:56:41 +0100

    Search videos Form

--- src/App.js
+++ src/App.js
@@ -8,10 +8,19 @@ class App extends Component {
 	constructor(props) {
 		super(props)
 
-		this.state = { videos: [] }
+		this.state = { 
+			searchTerm: "",
+			videos: []
+		}
+	}
+
+	/* Video searching using state.searchTerm */
+	searchVideos() {
+		
+		console.log('Searching Videos:' + this.state.searchTerm)
 
 		const searchApi = "https://www.googleapis.com/youtube/v3/search"
-		const queryTerm = encodeURIComponent("donuts hole")
+		const queryTerm = encodeURIComponent(this.state.searchTerm)
 		const url = searchApi + "?q=" + queryTerm + "&key=" + apiKey + "&maxResults=10&part=snippet"
 
 		axios.get(url)
@@ -19,19 +28,41 @@ class App extends Component {
 
 				const videos = response.data.items
 					.filter(v => v.id.kind === "youtube#video")
-					.map(v => ({id: v.id.videoId, title: v.snippet.title}))
+					.map(v => ({
+						id: v.id.videoId, 
+						title: v.snippet.title,
+						image: v.snippet.thumbnails.medium
+					}))
 
 				this.setState({ videos: videos })
 			})
 			.catch((error) => {
 				console.error(error)
 			})
+	}
 
+	/* Updating state.searchTerm */
+	updateSearchTerm(value) {
+
+		this.setState({ searchTerm: value })
 	}
 
 	render() {
 
-		return <VideoList videos={this.state.videos}></VideoList>
+		return (
+			<div>
+				<input 
+					value= {this.state.searchTerm} 
+					placeholder="Search Video" 
+					onChange={(event) => this.updateSearchTerm(event.target.value)}
+				/>
+				<button 
+					onClick={(event) => this.searchVideos()}>
+					Search!
+				</button>
+				<VideoList videos={this.state.videos}></VideoList>
+			</div>
+		)
 	}
 }
 
